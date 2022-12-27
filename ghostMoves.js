f009d5f70d3b61d9d6df074294eebfd35b10b75d
Vinoth-KR <86417728+Vinoth-KR@users.noreply.github.com>
2022-12-27 20:07:31 +0530

    First Commit

---
+++ ghostMoves.js
@@ -0,0 +1,22 @@
+import { DIRECTIONS, OBJECT_TYPE } from "./setup";
+
+
+export function randomMovement(position, direction, objectExist){
+    let dir = direction;
+    let nextMovePos = position + dir.movement;
+    
+    const keys = Object.keys(DIRECTIONS);
+
+    while(objectExist(nextMovePos, OBJECT_TYPE.WALL) ||
+           objectExist(nextMovePos, OBJECT_TYPE.GHOST)
+    ){
+        const key = keys[Math.floor(Math.random() * keys.length)];
+
+        //set the next move and direction.
+        dir = DIRECTIONS[key];
+
+        nextMovePos = position + dir.movement;
+    }
+
+    return {nextMovePos, direction: dir};
+}
